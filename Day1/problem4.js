0ac0dfc86055f2bcfb338a379b5931de6f67c8a1
Shantanu <sutharshantanu@gmail.com>
2022-09-03 17:43:35 +0530

    Step 1

---
+++ Day1/problem4.js
@@ -0,0 +1,21 @@
+// Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+// Print the report card of the student (You can make it look nice by using some keyboard symbols )
+// Explore ASCII ART (https://en.wikipedia.org/wiki/ASCII_art (Links to an external site.)) or Text Art (https://fsymbols.com/text-art/ (Links to an external site.)) for some inspiration
+
+let schoolName = "GGS Public Sen. Secondary School";
+let type = "Roll No :", name = "Full Name", sec = "Section";
+let rollno = 2750459, fullname = "Shantanu", Grade = "Grade B+", section = "Commerce";
+
+let eng = "English = 45", math = "Math = 48", hindi = "Hindi = 65";
+
+let total = "Total Marks = 158";
+
+console.log(schoolName);
+console.log(type, name, sec)
+console.log(rollno, fullname, section);
+
+console.log(eng);
+console.log(math);
+console.log(hindi);
+
+console.log(total,Grade);
